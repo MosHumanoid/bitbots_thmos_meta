35126ade2339b2364a39f7acb31c8d39757f109a
jinyin-z19 <19807733522@163.com>
2023-11-28 12:49:16 +0800

    update thmos_bringup

--- thmos_bringup/setup.py
+++ thmos_bringup/setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['bitbots_bringup'],
-    #scripts=['bin/myscript'],
+    packages=['thmos_bringup'],
     package_dir={'': 'src'}
 )
 
